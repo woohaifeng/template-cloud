e39699cc2757c64d4c8becb33ced1b2c925654fd
woohaifeng <woohaifeng@hotmail.com>
2019-07-29 17:03:48 +0800

    Eureka

--- service-redis/src/main/java/com/company/service/redis/controller/RedisController.java
+++ service-redis/src/main/java/com/company/service/redis/controller/RedisController.java
@@ -5,20 +5,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
 public class RedisController {
     @Autowired
     private RedisService redisService;
 
+    /**
+     * 缓存数据
+     * @param key
+     * @param value
+     * @param seconds
+     * @return
+     */
     @PostMapping("/put")
     public String put(String key,String value,long seconds) {
         redisService.put(key,value,seconds);
         return "ok";
     }
 
+    /**
+     * 取缓存
+     * @param key
+     * @return
+     */
     @PostMapping("/get")
     public Object get(String key) {
         Object o = redisService.get(key);

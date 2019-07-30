a0e7185707a68cf1b9cb0218eeeaa70e8173be83
woohaifeng <woohaifeng@hotmail.com>
2019-07-30 17:18:29 +0800

    Eureka

---
+++ common-service/src/main/java/com/company/common/service/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.company.common.service.mapper;
+
+import com.company.common.service.domain.User;
+import tk.mybatis.mapper.MMapper;
+
+public interface UserMapper extends MMapper<User> {
+}
